a963951bd705754c059d9a9d6deffc296a5c612f
Lechiffre2110 <73186756+Lechiffre2110@users.noreply.github.com>
2023-01-24 00:53:22 +0100

    feature: add screen saver

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Click : MonoBehaviour
+{
+    public VideoPlayer MyVideoPlayer;
+    int IdleTimeSetting = 3;
+    float LastIdleTime;
+    public Canvas canvas;
+
+    private void Start()
+    {
+        MyVideoPlayer.enabled = true;
+        MyVideoPlayer.isLooping = true;
+
+    }
+
+    void Awake()
+    {
+
+        LastIdleTime = Time.time;
+    }
+
+    private void Update()
+    {
+
+        if (Input.anyKey)
+        {
+            LastIdleTime = Time.time;
+            MyVideoPlayer.enabled = false;
+            canvas.enabled = true;
+        }
+
+        if (IdleCheck())
+        {
+            canvas.enabled = false;
+            MyVideoPlayer.enabled = true;
+        }
+    }
+
+    public bool IdleCheck()
+    {
+        return Time.time - LastIdleTime > IdleTimeSetting;
+    }
+
+}
